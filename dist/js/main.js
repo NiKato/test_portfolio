a9b74b61d7fd2f06131bed4a5b20e532c29faeb6
NiKato <nikolakato@gmail.com>
2019-10-22 11:34:15 +0200

    'initial'

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,27 @@
+// Loader
+
+var loader;
+function loadNow(opacity) {
+  if (opacity <= 0) {
+    displayContent();
+  } else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function() {
+      loadNow(opacity - 0.04);
+    }, 100);
+  }
+}
+
+function displayContent() {
+  loader.style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  loader = document.getElementById("loader");
+  loadNow(1);
+});
+
 // Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
@@ -31,3 +55,4 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
